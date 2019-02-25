e3ee08d5888c37494faf34d3ff4472115f7255cc
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-25 14:11:13 -0800

    Move code to src folder.

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Message } from "../components/Message";
-import { GridWireFrame } from "../components/GridWireFrame";
+import { Message } from "../src/Message";
+import { GridWireFrame } from "../src/GridWireFrame";
 
 export default function IndexPage() {
   return (
