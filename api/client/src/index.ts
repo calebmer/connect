eabd26117c9f286e267bf47645bc8b14821930b7
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-26 14:28:36 -0800

    Add documentation for API methods.

--- api/client/src/index.ts
+++ api/client/src/index.ts
@@ -1,5 +1,10 @@
 import * as Schema from "./Schema";
 
+/**
+ * Creates a new account with an email and a password. Accounts may have any
+ * string as their name even if another account has the same name. That name can
+ * be updated at any time and is used for display purposes.
+ */
 export const signUp = Schema.createMutation({
   path: "/account/sign-up",
   input: {
@@ -10,6 +15,10 @@ export const signUp = Schema.createMutation({
   output: {},
 });
 
+/**
+ * Allows a user to sign in to their account with the password they selected
+ * when creating their account.
+ */
 export const signIn = Schema.createMutation({
   path: "/account/sign-in",
   input: {
@@ -19,6 +28,11 @@ export const signIn = Schema.createMutation({
   output: {},
 });
 
+/**
+ * Allows a user to sign out of their account on their current device. To access
+ * the private information associated with their account again the user must
+ * sign back in.
+ */
 export const signOut = Schema.createMutation({
   path: "/account/sign-out",
   input: {},
