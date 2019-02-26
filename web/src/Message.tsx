b77c30cf23a8b521e8243805ecf65256b5ec4002
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-26 10:12:20 -0800

    Rename signature component.

--- web/src/Message.tsx
+++ web/src/Message.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { BodyText } from "./Text";
-import { Signature } from "./Signature";
+import { ProfileSignature } from "./ProfileSignature";
 
 export function Message() {
   return (
-    <Signature>
+    <ProfileSignature>
       <BodyText>
         and on press next on page3, you would manipulate state to change page#
         or any other variable that would then make it show page 4. same for page
         5. now if someone presses back on any of 3,4,5, they would end up on
         page 2
       </BodyText>
-    </Signature>
+    </ProfileSignature>
   );
 }
--- web/src/ProfileSignature.tsx
+++ web/src/ProfileSignature.tsx
@@ -10,7 +10,7 @@ import { lineHeight } from "./StyleConstants";
  * content was created. The user’s profile image is put on the left and their
  * name is included above the body content.
  */
-export function Signature({ children }: { children: ReactNode }) {
+export function ProfileSignature({ children }: { children: ReactNode }) {
   return (
     <View style={styles.container}>
       <ProfileImage />
