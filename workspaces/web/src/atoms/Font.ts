35516f008db63f8446bd57a902343fe5653cbf7a
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-28 15:33:22 -0800

    Add color and font atoms.

---
+++ workspaces/web/src/atoms/Font.ts
@@ -0,0 +1,57 @@
+// Font families and variations:
+export const serif = {
+  fontFamily: "freight-text-pro, serif",
+  fontWeight: "400",
+  fontStyle: "normal",
+};
+export const serifBold = {
+  fontFamily: "freight-text-pro, serif",
+  fontWeight: "700",
+  fontStyle: "normal",
+};
+export const serifItalic = {
+  fontFamily: "freight-text-pro, serif",
+  fontWeight: "400",
+  fontStyle: "italic",
+};
+export const serifBoldItalic = {
+  fontFamily: "freight-text-pro, serif",
+  fontWeight: "700",
+  fontStyle: "italic",
+};
+export const sans = {
+  fontFamily: "freight-sans-pro, sans-serif",
+  fontWeight: "400",
+  fontStyle: "normal",
+};
+export const sansBold = {
+  fontFamily: "freight-sans-pro, sans-serif",
+  fontWeight: "700",
+  fontStyle: "normal",
+};
+
+// Type size scale:
+export const size0 = {
+  fontSize: 12,
+  lineHeight: 18,
+};
+export const size1 = {
+  fontSize: 14,
+  lineHeight: 21,
+};
+export const size2 = {
+  fontSize: 16,
+  lineHeight: 24,
+};
+export const size3 = {
+  fontSize: 18,
+  lineHeight: 24,
+};
+export const size4 = {
+  fontSize: 24,
+  lineHeight: 24,
+};
+export const size5 = {
+  fontSize: 36,
+  lineHeight: 36,
+};
