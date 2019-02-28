87ece1d3266dce5a84717a8cf7a3a6e4204e111a
Caleb Meredith <calebmeredith8@gmail.com>
2019-02-28 15:40:57 -0800

    Add space atomic values.

---
+++ workspaces/web/src/atoms/Space.ts
@@ -0,0 +1,18 @@
+// Horizontal and vertical spacing scale:
+export const space0 = 4;
+export const space1 = 8;
+export const space2 = 12;
+export const space3 = 16;
+export const space4 = 24;
+export const space5 = 32;
+export const space6 = 48;
+export const space7 = 64;
+export const space8 = 96;
+export const space9 = 128;
+export const space10 = 192;
+export const space11 = 256;
+export const space12 = 384;
+// Intentionally skip space13 😉
+export const space14 = 512;
+export const space15 = 640;
+export const space16 = 768;
---
+++ workspaces/web/src/atoms/index.ts
@@ -0,0 +1,5 @@
+import * as Color from "./Color";
+import * as Font from "./Font";
+import * as Space from "./Space";
+
+export {Color, Font, Space};
