ddbdd23f15af187c01989600cf609c634d23fece
Caleb Meredith <calebmeredith8@gmail.com>
2019-03-05 14:40:08 -0800

    WIP routes.

--- app/src/router/Routes.ts
+++ app/src/router/Routes.ts
@@ -1,12 +1,46 @@
 import React from "react";
 
-createStack({
-  SignIn: React.lazy(() => import("../SignIn").then(m => m.SignIn)),
-  SignUp: React.lazy(() => import("../SignUp").then(m => m.SignUp)),
-});
+// const place = create({
+//   routes: {
+//     SignIn: {
+//       component: () => import("../SignIn").then(m => m.SignIn),
+//     },
+//     SignUp: {
+//       component: () => import("../SignUp").then(m => m.SignUp),
+//     },
+//   },
+// });
 
-function createStack<
-  Routes extends {
-    readonly [key: string]: () => Promise<React.ComponentType<{}>>;
-  }
->(routes: Routes) {}
+// place.router.SignIn.push({x: 42});
+
+// function create<RouterConfig extends StackRouterConfigBase>(
+//   routes: RouterConfig,
+// ): {
+//   readonly router: StackRouter<RouterConfig["routes"]>;
+// } {
+//   return null as any;
+// }
+
+// type StackRouterConfigBase = {
+//   readonly routes: {
+//     readonly [key: string]: StackRouteConfigBase;
+//   };
+// };
+
+// type StackRouteConfigBase = {
+//   readonly component: () => Promise<React.ComponentType<never>>;
+// };
+
+// type StackRouteProps<
+//   Route extends StackRouteConfigBase
+// > = Route["component"] extends () => Promise<React.ComponentType<infer Props>>
+//   ? Props
+//   : never;
+
+// type StackRouter<Routes extends StackRouterConfigBase["routes"]> = {
+//   [Key in keyof Routes]: StackRouterRoute<Routes[Key]>
+// };
+
+// type StackRouterRoute<Route extends StackRouteConfigBase> = {
+//   readonly push: (props: StackRouteProps<Route>) => void;
+// };
