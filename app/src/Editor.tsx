9144d51c02dae126335a447b28b4f2da0c0235c5
Caleb Meredith <calebmeredith8@gmail.com>
2019-03-06 17:43:06 -0800

    Add editor component.

---
+++ app/src/Editor.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {View} from "react-native";
+
+/**
+ * A rich text editor for creating or updating text-based content. The editor
+ * is consistent across all areas of our application.
+ *
+ * - Composing posts.
+ * - Editing comments.
+ * - Sending messages.
+ */
+export function Editor() {
+  return <View />;
+}
