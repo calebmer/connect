eb7d235e198d5065bc9675c770158c098d8c0ab7
Caleb Meredith <calebmeredith8@gmail.com>
2019-03-10 15:00:25 -0700

    Add scroll view for group route.

--- app/src/Group.tsx
+++ app/src/Group.tsx
@@ -1,26 +1,83 @@
-import {StyleSheet, View} from "react-native";
+import {
+  Animated,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Platform,
+  StyleSheet,
+  View,
+} from "react-native";
+import React, {useState} from "react";
+import {Color} from "./atoms";
 import {GroupBanner} from "./GroupBanner";
 import {GroupPostPrompt} from "./GroupPostPrompt";
-import React from "react";
 
 export function Group() {
+  // On iOS you can scroll up which results in a negative value for `scrollY`.
+  // When that happens we want to scale up our group banner so that it
+  // fills in the extra space. That’s what the `bannerScale` value is for. It
+  // translates a negative scroll offset into a scale transformation.
+  //
+  // There’s some weirdness on iOS where where `scrollY` starts at some negative
+  // value like -44 on an iPhone X instead of 0, so we record the first value of
+  // `scrollY` and use it as an offset.
+  const [scrollY] = useState(new Animated.Value(0));
+  const [offsetScrollY, setOffsetScrollY] = useState<null | number>(null);
+  const bannerScale =
+    offsetScrollY === null
+      ? 1
+      : scrollY.interpolate({
+          inputRange: [-GroupBanner.height, 0].map(y => y + offsetScrollY),
+          outputRange: [2.7, 1], // NOTE: I would expect this number to be 2 and not 2.7, but experimental evidence proves otherwise.
+          extrapolateLeft: "extend",
+          extrapolateRight: "clamp",
+        });
+
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
+      <Animated.View
+        style={[styles.banner, {transform: [{scale: bannerScale}]}]}
+      >
         <GroupBanner />
-        <GroupPostPrompt />
-      </View>
+      </Animated.View>
+      <Animated.ScrollView
+        scrollEventThrottle={16}
+        onScrollBeginDrag={(event: NativeSyntheticEvent<NativeScrollEvent>) => {
+          if (offsetScrollY === null) {
+            setOffsetScrollY(event.nativeEvent.contentOffset.y);
+          }
+        }}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {useNativeDriver: Platform.OS !== "web"},
+        )}
+      >
+        <View style={styles.content}>
+          <GroupPostPrompt />
+          <View style={{height: 1000}} />
+          <GroupPostPrompt />
+        </View>
+      </Animated.ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    overflow: "hidden",
     flex: 1,
-    alignItems: "center",
-  },
-  card: {
+    position: "relative",
     width: "100%",
     maxWidth: GroupBanner.maxWidth,
+    backgroundColor: Color.grey1,
+  },
+  banner: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  content: {
+    marginTop: GroupBanner.height,
+    backgroundColor: Color.grey1,
   },
 });
