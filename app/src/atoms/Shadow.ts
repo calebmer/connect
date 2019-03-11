0531fe06756c213e024c5fe156fb98be1167dc76
Caleb Meredith <calebmeredith8@gmail.com>
2019-03-11 13:20:41 -0700

    Fork shadows

--- app/src/atoms/Shadow.ts
+++ app/src/atoms/Shadow.ts
@@ -1,36 +1,78 @@
+import {Platform} from "react-native";
+
 // TODO: Android elevation.
 
-export const elevation0 = {
-  shadowOffset: {width: 0, height: 1},
-  shadowRadius: 3,
-  shadowColor: "black",
-  shadowOpacity: 0.2,
-};
+export const elevation0 = Platform.select({
+  ios: {
+    shadowOffset: {width: 0, height: 1},
+    shadowRadius: 3,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+  web: {
+    shadowOffset: {width: 0, height: 1},
+    shadowRadius: 3,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+});
 
-export const elevation1 = {
-  shadowOffset: {width: 0, height: 4},
-  shadowRadius: 6,
-  shadowColor: "black",
-  shadowOpacity: 0.2,
-};
+export const elevation1 = Platform.select({
+  ios: {
+    shadowOffset: {width: 0, height: 4},
+    shadowRadius: 6,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+  web: {
+    shadowOffset: {width: 0, height: 4},
+    shadowRadius: 6,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+});
 
-export const elevation2 = {
-  shadowOffset: {width: 0, height: 5},
-  shadowRadius: 15,
-  shadowColor: "black",
-  shadowOpacity: 0.2,
-};
+export const elevation2 = Platform.select({
+  ios: {
+    shadowOffset: {width: 0, height: 5},
+    shadowRadius: 15,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+  web: {
+    shadowOffset: {width: 0, height: 5},
+    shadowRadius: 15,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+});
 
-export const elevation3 = {
-  shadowOffset: {width: 0, height: 10},
-  shadowRadius: 24,
-  shadowColor: "black",
-  shadowOpacity: 0.2,
-};
+export const elevation3 = Platform.select({
+  ios: {
+    shadowOffset: {width: 0, height: 10},
+    shadowRadius: 24,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+  web: {
+    shadowOffset: {width: 0, height: 10},
+    shadowRadius: 24,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+});
 
-export const elevation4 = {
-  shadowOffset: {width: 0, height: 15},
-  shadowRadius: 35,
-  shadowColor: "black",
-  shadowOpacity: 0.2,
-};
+export const elevation4 = Platform.select({
+  ios: {
+    shadowOffset: {width: 0, height: 15},
+    shadowRadius: 35,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+  web: {
+    shadowOffset: {width: 0, height: 15},
+    shadowRadius: 35,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+  },
+});
