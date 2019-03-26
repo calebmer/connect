2ae7aedc8932f72634895b1c8a03a22665a37ee8
Caleb Meredith <calebmeredith8@gmail.com>
2019-03-25 18:38:25 -0700

    Delete ChevronRightIcon.

--- app/src/icons/ChevronRightIcon.native.tsx
+++
@@ -1,5 +0,0 @@
-import {createIconComponent} from "./Icon.native";
-
-export const ChevronRightIcon = createIconComponent(
-  require("./chevron-right.png"),
-);
--- app/src/icons/ChevronRightIcon.web.tsx
+++
@@ -1,5 +0,0 @@
-import {createIconComponent} from "./Icon.web";
-
-export const ChevronRightIcon = createIconComponent(
-  require("./chevron-right.react.svg"),
-);
