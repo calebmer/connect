8b5cc346d7229489865b85325c48d0725f89cb1f
Caleb Meredith <calebmeredith8@gmail.com>
2019-03-27 11:11:47 -0700

    When group home is not provided a post then redirect to the first post in the group.

--- app/src/GroupHomeWeb.web.tsx
+++ app/src/GroupHomeWeb.web.tsx
@@ -1,10 +1,14 @@
 import {Color, Shadow, Space} from "./atoms";
 import {Platform, StyleSheet, View} from "react-native";
+import React, {useEffect} from "react";
 import {Group} from "./Group";
+import {GroupCache} from "./cache/GroupCache";
 import {Post} from "./Post";
 import {PostID} from "@connect/api-client";
-import React from "react";
+import {PostRoute} from "./router/AllRoutes";
 import {Route} from "./router/Route";
+import {useCacheData} from "./cache/framework/Cache";
+import {useCacheListData} from "./cache/framework/CacheList";
 
 if (Platform.OS !== "web") {
   throw new Error("Can only use this module on the web.");
@@ -13,17 +17,37 @@ if (Platform.OS !== "web") {
 export function GroupHomeWeb({
   route,
   groupSlug,
-  postID: _postID,
+  postID: _actualPostID,
 }: {
   route: Route;
   groupSlug: string;
   postID?: string;
 }) {
+  // Parse a post ID from our props which comes from the URL.
+  const actualPostID =
+    _actualPostID != null ? (parseInt(_actualPostID, 10) as PostID) : undefined;
+
+  // Get the list of posts for this component.
+  const {postCacheList} = useCacheData(GroupCache, groupSlug);
+  const posts = useCacheListData(postCacheList);
+
+  // If we weren’t provided a post ID then use the ID of the first post in
+  // our group.
   const postID =
-    _postID != null ? (parseInt(_postID, 10) as PostID) : undefined;
+    actualPostID == null && posts.length > 0 ? posts[0].id : actualPostID;
+
+  // If we are rendering a different post ID then the one we were provided,
+  // update our route so that it points to the actual post we are rendering.
+  useEffect(() => {
+    if (actualPostID !== postID) {
+      route.push(PostRoute, {groupSlug, postID: String(postID)});
+    }
+  }, [actualPostID, groupSlug, postID, route]);
 
   return (
     <View style={styles.container}>
+      {/* TODO: Instead of re-rendering `<Group>` whenever the URL changes we
+          could use context. */}
       <Group route={route} groupSlug={groupSlug} selectedPostID={postID} />
       <View style={styles.content}>
         {postID != null && (
