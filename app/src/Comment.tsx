66d804f3c71b299beab6bc67efb9b788f607a710
Caleb Meredith <calebmeredith8@gmail.com>
2019-04-02 10:56:33 -0400

    Create AccountByline component and use it for comments.

--- app/src/Comment.tsx
+++ app/src/Comment.tsx
@@ -1,11 +1,35 @@
+import {BodyText, Space} from "./atoms";
+import {StyleSheet, View} from "react-native";
+import {AccountAvatar} from "./AccountAvatar";
+import {AccountByline} from "./AccountByline";
+import {AccountCache} from "./cache/AccountCache";
 import {CommentCache} from "./cache/CommentCache";
 import {CommentID} from "@connect/api-client";
 import React from "react";
-import {Text} from "react-native";
 import {useCacheData} from "./cache/framework/Cache";
 
 export function Comment({commentID}: {commentID: CommentID}) {
   const comment = useCacheData(CommentCache, commentID);
+  const author = useCacheData(AccountCache, comment.authorID);
 
-  return <Text>{comment.content}</Text>;
+  return (
+    <View style={styles.comment}>
+      <AccountAvatar account={author} />
+      <View style={styles.body}>
+        <AccountByline account={author} time={comment.postedAt} />
+        <BodyText>{comment.content}</BodyText>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  comment: {
+    flexDirection: "row",
+    padding: Space.space3,
+  },
+  body: {
+    flex: 1,
+    paddingLeft: Space.space3,
+  },
+});
--- app/src/GroupItemFeed.tsx
+++ app/src/GroupItemFeed.tsx
@@ -1,13 +1,13 @@
-import {BodyText, Font, LabelText, MetaText} from "./atoms";
-import {Platform, StyleSheet, View} from "react-native";
+import {BodyText, Font} from "./atoms";
+import {GroupHomeLayout, GroupHomeLayoutContext} from "./GroupHomeLayout";
+import React, {useContext} from "react";
+import {AccountByline} from "./AccountByline";
 import {AccountCache} from "./cache/AccountCache";
 import {GroupItem} from "./GroupItem";
 import {PostCache} from "./cache/PostCache";
 import {PostID} from "@connect/api-client";
 import {PostRoute} from "./router/AllRoutes";
-import React from "react";
 import {Route} from "./router/Route";
-import {communicateTime} from "./communicateTime";
 import {useCacheData} from "./cache/framework/Cache";
 
 function GroupItemFeed({
@@ -25,12 +25,10 @@ function GroupItemFeed({
   const post = useCacheData(PostCache, postID);
   const account = useCacheData(AccountCache, post.authorID);
 
-  // NOTE: It’s a side-effect to call `new Date()` in render! Ideally, we would
-  // have a hook that subscribes to the system time and re-renders this
-  // component when it changes. At the moment, it’s not a big issue. This
-  // component is memoized anyway so the current date only changes when
-  // the props change.
-  const publishedAt = communicateTime(new Date(), new Date(post.publishedAt));
+  // On a laptop, you can open a post without leaving the feed. So favor shorter
+  // post previews and more posts on screen.
+  const numberOfLines =
+    useContext(GroupHomeLayoutContext) === GroupHomeLayout.Laptop ? 2 : 4;
 
   return (
     <GroupItem
@@ -38,11 +36,11 @@ function GroupItemFeed({
       selected={selected}
       onPress={() => route.push(PostRoute, {groupSlug, postID: String(postID)})}
     >
-      <View style={styles.header}>
-        <LabelText>{account.name}</LabelText>
-        <MetaText style={styles.publishedAt}>{publishedAt}</MetaText>
-      </View>
-      <BodyText style={styles.text} numberOfLines={numberOfLines}>
+      <AccountByline account={account} time={post.publishedAt} />
+      <BodyText
+        style={{maxHeight: Font.size2.lineHeight * numberOfLines}}
+        numberOfLines={numberOfLines}
+      >
         {post.content}
       </BodyText>
     </GroupItem>
@@ -51,30 +49,3 @@ function GroupItemFeed({
 
 const GroupItemFeedMemo = React.memo(GroupItemFeed);
 export {GroupItemFeedMemo as GroupItemFeed};
-
-// On web, you can open a post without leaving the feed. So favor shorter post
-// previews and more posts on screen.
-const numberOfLines = Platform.OS === "web" ? 2 : 4;
-
-const styles = StyleSheet.create({
-  header: {
-    flexDirection: "row",
-
-    // NOTE: `baseline` doesn’t work very well outside of web. Super sad!
-    alignItems: Platform.OS === "web" ? "baseline" : undefined,
-  },
-  publishedAt: {
-    marginLeft: 6,
-    ...Platform.select({
-      web: {},
-      default: {
-        // NOTE: Manually align to the baseline since `alignItems: "baseline"`
-        // doesn’t do it.
-        bottom: -2.6,
-      },
-    }),
-  },
-  text: {
-    maxHeight: Font.size2.lineHeight * numberOfLines,
-  },
-});
