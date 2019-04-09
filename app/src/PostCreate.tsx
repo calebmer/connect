8d55a09d4b4bd47453cda8c99154ede32f449c7b
Caleb Meredith <calebmeredith8@gmail.com>
2019-04-09 16:31:46 -0700

    Add header to post creation.

--- app/src/PostCreate.tsx
+++ app/src/PostCreate.tsx
@@ -1,9 +1,17 @@
-import {Color, Space} from "./atoms";
-import {Keyboard, KeyboardEvent, Platform, View} from "react-native";
+import {Color, LabelText, Space} from "./atoms";
+import {
+  Keyboard,
+  KeyboardEvent,
+  Platform,
+  StyleSheet,
+  View,
+} from "react-native";
 import React, {useEffect, useState} from "react";
+import {AccountAvatar} from "./AccountAvatar";
 import {Editor} from "./Editor";
 import {NavbarNativeScrollView} from "./NavbarNativeScrollView";
 import {Route} from "./router/Route";
+import {useCurrentAccount} from "./cache/AccountCache";
 
 export function PostCreate({route}: {route: Route}) {
   // When entering new content in a `UITextView`, iOS will scroll any parent
@@ -20,6 +28,7 @@ export function PostCreate({route}: {route: Route}) {
         useTitle={() => "New Post"}
         contentInset={iosInsetHack ? {bottom: Space.space3} : undefined} // Like padding but used when scrolling in response to text input changes.
       >
+        <PostCreateHeader />
         <Editor autoFocus scrollDisabled noPaddingBottom={iosInsetHack} />
       </NavbarNativeScrollView>
 
@@ -32,6 +41,18 @@ export function PostCreate({route}: {route: Route}) {
         }}
       />
     </>
+  );
+}
+
+function PostCreateHeader() {
+  const currentAccount = useCurrentAccount();
+  return (
+    <View style={styles.header}>
+      <AccountAvatar account={currentAccount} />
+      <View style={styles.byline}>
+        <LabelText>{currentAccount.name}</LabelText>
+      </View>
+    </View>
   );
 }
 
@@ -61,3 +82,14 @@ function useKeyboardHeight() {
 
   return keyboardHeight;
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    padding: Space.space3,
+    paddingBottom: 0,
+  },
+  byline: {
+    paddingLeft: Space.space3,
+  },
+});
