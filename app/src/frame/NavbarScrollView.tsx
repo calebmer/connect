5c07557b191e8d2553185830082f37f85d9478e9
Caleb Meredith <calebmeredith8@gmail.com>
2019-05-07 17:55:37 -0400

    Fix lint warning.

--- app/src/frame/NavbarScrollView.tsx
+++ app/src/frame/NavbarScrollView.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import {NavbarLayout, NavbarLayoutProps} from "./NavbarLayout";
 import {ScrollView, ScrollViewProps} from "react-native";
+import React from "react";
 
 export const NavbarScrollView: React.ComponentType<
   NavbarLayoutProps & ScrollViewProps & React.RefAttributes<ScrollView>
