cde5a491f8ec80b1e42300dc90e44de6c468c16b
Caleb Meredith <calebmeredith8@gmail.com>
2019-05-29 17:41:29 -0700

    Add script for serving our API proxy.

---
+++ app/web/scripts/proxy.js
@@ -0,0 +1,28 @@
+const url = require("url");
+const http = require("http");
+const chalk = require("chalk");
+const {proxyRequest, proxyUpgrade} = require("../lib/APIProxy");
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 7000;
+
+const API_URL = (() => {
+  if (!process.env.API_URL) {
+    throw new Error("Expected an API URL.");
+  }
+  return url.parse(process.env.API_URL);
+})();
+
+const server = http.createServer((req, res) => {
+  proxyRequest(API_URL, req, res);
+});
+
+server.on("upgrade", (req, socket, firstPacket) => {
+  proxyUpgrade(API_URL, req, socket, firstPacket);
+});
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `Listening on ${chalk.bold.underline(`http://localhost:${PORT}`)}`,
+  );
+});
