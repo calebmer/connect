3fb97325cbd95f036d3233c04b907d4642c1b9b7
Caleb Meredith <calebmeredith8@gmail.com>
2019-06-10 12:38:00 -0700

    Add new concern after measuring IDs.

--- api/client/src/generateID.ts
+++ api/client/src/generateID.ts
@@ -14,9 +14,9 @@
  *
  * Our ID representation mixes the Firebase and ULID representation.
  *
- * - 120 bits total
+ * - 108 bits total
  * - 48 bit timestamp (in milliseconds)
- * - 72 bits of randomness
+ * - 60 bits of randomness
  *
  * We use [Crockford’s base32][5] lowercase encoding. This alphabet excludes
  * I, L, O, and U to avoid confusion and abuse with obscene words. We use
@@ -33,6 +33,18 @@
  *   that are out of sync then they may generate out-of-order IDs. We can
  *   mitigate this by asking our API to send us a timestamp so we can sync up
  *   our client clock.
+ *
+ * - We use 60 bits of randomness whereas ULID uses 80 and Firebase uses 72.
+ *   initially when writing this algorithm, I (@calebmer) thought I was using
+ *   72 bits of randomness like Firebase. However, I did the math in the
+ *   implementation wrong. I was counting base 64 encoded bits for 12 characters
+ *   (bits in a base 64 character: 2 ^ 6 = 64, bits in 12 base 64 characters: 6 * 12 = 72)
+ *   instead of base 32 encoded bits for 12 characters (bits in a base 32
+ *   character: 2 ^ 5 = 32, bits in 12 base 32 characters: 5 * 12 = 60). Since
+ *   that mistake, this algorithm has been shipped to production and numerous places
+ *   in the code expect an ID that is 22 characters long. For now, I’m fine with
+ *   tolerating only 60 bits of randomness. If we ever start seeing collisions then
+ *   maybe it will be time to reconsider the algorithm.
  *
  * [1]: https://en.wikipedia.org/wiki/German_tank_problem
  * [2]: https://firebase.googleblog.com/2015/02/the-2120-ways-to-ensure-unique_68.html
